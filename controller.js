98ffb58b4495d432e30d19a73b7cbf435c0d9b73
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-05-07 02:21:20 +0600

    dynamic route

---
+++ controller.js
@@ -0,0 +1,10 @@
+export const usernameController =(res,req)=>{
+    const username = req.params.username;
+    res.send(`Welcome ${username}`)
+
+}
+
+export const searchController =(req,res)=>{
+    const keyword=req.query.keyword;
+    res.send(`searching for ${keyword}`);
+  }
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express");
+import { usernameController, searchController } from "./controller.js";
+
+import express from "express";
+
 const app = express();
 const PORT = 3000;
 //Define a simple route
 app.get("/", (req, res) => {
   res.send("Hello, !");
-});
+})
+
+app.get("/user/:username", usernameController);
+
+app.get("/search", searchController);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
