661d924d50d63d39262ebf181dd5cd3dd7720753
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-05-15 00:14:05 +0600

    image

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import express from "express";
 import router from "./route.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, file.filename + "-" + Date.now() + file.originalname);
+  },
+});
 
 const app = express();
+const upload = multer({ storage: storage });
 const PORT = 3000;
 
-//set EJs as the view engine
-app.use('/public',express.static("public"));
-app.use('/images',express.static("images"));
-
-app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.single("image"));
 
 app.get("/", (req, res) => {
   res.send("HELLO EXPRESS");
+});
+app.post("/form", (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("From Recived");
 });
 
 app.listen(PORT, () => {
