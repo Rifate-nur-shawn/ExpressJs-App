04446b2a633b0ad96508ab7c990fa450f5049d13
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-05-14 23:40:09 +0600

    p

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 import express from "express";
-
 import router from "./route.js";
 
 const app = express();
 const PORT = 3000;
+
+//set EJs as the view engine
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const userName = "mark";
+  res.render("index", { userName });
 });
-app.use('/user', router);
 
-app.post('/users',express.json(),(req,res)=>{
-  const {name , email} =req.body
-  res.json({
-    massage:`User ${name}with email ${email} created successfully`
-  })
-})
-app.get('/things/:name/:id',(req,res)=>{
-  const {name,id}=req.params
-    res.json({
-      id,
-      name
-    })
-})
-// app.delete ('')
-
-// Middleware to parse JSON bodies
-
-// app.put('/users/:id', (req, res) => {
-//   const  userId=req.params.id
-//   const { name, email } = req.body;
-//   res.json({
-//     message: `User with ID ${userId} updated successfully with name ${name},  ${email}`
-//   }); 
-// });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
