3b964d9f66f9b769131207664b8f7d1057c029f2
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-05-15 23:15:07 +0600

    save data in mongoDB database

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 import express from "express";
-import router from "./route.js";
-import multer from "multer";
+
 import { connectDB } from "./config/db.js";
-
-
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    cb(null, file.filename + "-" + Date.now() + file.originalname);
-  },
-});
+import { Person } from "./models/person.js";
 
 const app = express();
-const upload = multer({ storage: storage });
 const PORT = 3000;
 await connectDB();
-
-
- 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(upload.single("image"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("HELLO EXPRESS");
 });
-app.post('/person',express.json, (req, res) => {
-  console.log(req.body);
-  res.send("Person Recived");
+//saveing data in mongoDb
+app.post("/person", async (req, res) => {
+  const { email, name, age } = req.body;
+  const newPerson = new Person({
+    name,
+    age,
+    email,
+  });
+  await newPerson.save();
+  console.log(newPerson)
+  res.send("Person data received");
+});
+//updating data in mongoDb
+app.put("/person",  async (req, res) => {
+  const { id} = req.body;
+
+  const personData =await Person.findById(id);
+  personData.age =30
+  personData.save();
+
+  console.log(personData)
+  res.send("Person data updated");
 });
 
-app.post("/form", (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.send("From Recived");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
